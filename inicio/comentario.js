5bbbb8e1beb9de5469be0b6dc2c3d63e101b5776
Fábio Rocha <Fabiorocharb@gmail.com>
2022-09-10 07:58:00 -0300

    Erro de escrita.

--- inicio/comentario.js
+++ inicio/comentario.js
@@ -1,4 +1,4 @@
-// Console.log é usado para exibir algo na tela do computado!
+// Console.log é usado para exibir algo na tela do computador!
 console.log('Passo #01');
 console.log("Passo #02"); // Mais um exemplo de console.log
 console.log("Passo #03");
