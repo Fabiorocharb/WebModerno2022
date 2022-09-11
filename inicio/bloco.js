d26b4b7c84ad10eaaeeab5227e6a79b023846267
Fábio Rocha <Fabiorocharb@gmail.com>
2022-09-11 15:02:52 -0300

    blocos.js

---
+++ inicio/bloco.js
@@ -0,0 +1,26 @@
+//Bloco é onde vai agrupar sentenças de blocos.
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+ //Bloco de código deve ser aberto e fechado.
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
+//Bloco de código pode ser vazio.
+{
+    {
+        {
+            {
+                ;
+                ;
+                ;
+            }
+        }
+    }
+}
+//Mas não faz sentido criar blocos sem funções
+console.log("Fim!");
+//É ORGANIZADO POR PASTAS > ARQUIVOS > SENTEÇAS DE CODIGO/BLOCOS
---
+++ inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,23 @@
+console.log("Números impar:")
+{
+    console.log("1");
+    console.log("3");
+    console.log("5");
+    console.log("7");
+    console.log("9");
+
+}
+console.log("Número pares:")
+{
+    console.log("2");
+    console.log("4");
+    console.log("6");
+    console.log("8");
+    console.log("10");
+}
+
+//CRTL + // = selecionar ou tirar comentários selecionados;
+//CTRL + TAB =  Para frente;
+//CTRL + X = EXCLUIR LINHAS;
+//ALT + SHIFT + SETA PARA BAIXO = DUPLICA AS LINHAS VARIAS VEZES;
+//
