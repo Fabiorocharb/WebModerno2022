76fb99fe3f07f3a1d3c33a08aee192641a9bd9af
Fábio Rocha <Fabiorocharb@gmail.com>
2022-09-12 20:22:35 -0300

    String_Number_Boolean

---
+++ inicio/dados_3.js
@@ -0,0 +1,12 @@
+let idade = 30;
+console.log(typeof 30);
+console.log(typeof idade);    //TYPEOF SERVE PARA IDENTIFICAR O TIPO DA VARIAVEL
+
+let salario = 4578.32;
+console.log(typeof 4578.32); //Number - Qualquer variavel com número se chama.
+console.log(typeof salario);
+
+let estaChovendo = true // ou false - Boolean - verdadeiro ou falso
+console.log(typeof estaChovendo);       
+
+console.log(typeof "Teste"); // String - Sequência caracteres
