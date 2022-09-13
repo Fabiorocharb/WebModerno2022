61969a6a11ed5b3437538f55d7406943d62b65f2
Fábio Rocha <Fabiorocharb@gmail.com>
2022-09-13 07:38:42 -0300

    fundamentos.1

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,12 @@
+console.log("Sentença de código."); 
+
+{
+    {
+        console.log("Bom dia !");
+        console.log("Desenvolvedor!") // padrão do curso
+    }
+}
+// Como JS é organizado ?
+// - Organizado em setenças de código que podem termina ou não em ponto virgula e bloco de códigos
+// 3 setenças de código
+// 2 Blocos
