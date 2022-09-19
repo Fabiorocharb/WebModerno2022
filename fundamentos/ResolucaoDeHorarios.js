9599e62ac2cbb237b345e7714b80168ee93f3220
Fábio Rocha <Fabiorocharb@gmail.com>
2022-09-19 20:05:46 -0300

    resoluçãoDeHorarios

---
+++ fundamentos/ResolucaoDeHorarios.js
@@ -0,0 +1,7 @@
+console.log('Resolução de horários:')
+let dia = 24 ;
+let trabalho = 10 ; 
+const almoço = 1.5   
+const resultado = dia - trabalho - almoço
+
+console.log('Você tem', resultado ,'horas disponível, aproveite ela.')
