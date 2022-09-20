1581d452b5d17e783c307c361d3767cb6a1dfa22
Fábio Rocha <Fabiorocharb@gmail.com>
2022-09-19 23:26:23 -0300

    19/09/2022

---
+++ fundamentos/UsandoLet1.js
@@ -0,0 +1,8 @@
+var numero = 1
+{
+    let numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+
+// Quando se USA o LET sempre vai usar o que esta dentro
---
+++ fundamentos/usandoVAr2.js
@@ -0,0 +1,8 @@
+var numero = 1 
+{
+    var numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+
+//Quando é usando var e bloco separdo a var acumula o ultimo
