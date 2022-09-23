9aee3cd4144de85b9c216f85b47194e0499739c1
Fábio Rocha <Fabiorocharb@gmail.com>
2022-09-23 20:13:06 -0300

    Create VarLetConst.js

---
+++ fundamentos/VarLetConst.js
@@ -0,0 +1,11 @@
+let explicando = 'Com a evolução da linguagem surgiu o LET, sendo a versão atual.'
+console.log(explicando)
+
+explicando = 'Pórem ela so pode declarada uma vez no inicio.'
+console.log(explicando)
+
+let explicando = 'Caso fizer assim, vai dar ERRO'
+console.log(explicando)
+
+
+
