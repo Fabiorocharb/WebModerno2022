3fefcdd2fca0139f3bdd754c51c8a4740ee322dd
Fábio Rocha <Fabiorocharb@gmail.com>
2023-01-18 23:32:30 -0300

    Different.Idade

---
+++ Onebicode/JavaScript/helloword.js
@@ -0,0 +1,9 @@
+let old = prompt ("Digite o nome da pessoa mais velha:");
+let oldAge = prompt ("Digite a idade da pessoa mais velha:");
+
+let New = prompt ("Digite o nome da pessoa mais nova:");
+let NewAge = prompt ("Digite a idade da pessoa mais nova:")
+
+let different = oldAge - NewAge 
+
+alert("Pessoa mais velha:" + old + "\nIdade: " + oldAge + "\n\nPessoa mais nova: "+ New + "\nIdade: " + NewAge + "\n\nDiferença de idade: " + different)
